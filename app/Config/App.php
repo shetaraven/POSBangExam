3ecc06a0bc8541eb98b9ef1d83adf31c48fde85b
shetaraven <68856022+shetaraven@users.noreply.github.com>
2021-03-08 09:29:34 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8888/POSBangExam';
+	public $baseURL = 'https://posbangexam.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
